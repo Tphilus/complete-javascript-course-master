682d08181fde7cc1fd517ae8d79e3747b5401177
Tphilus <tkdadzie@st.ug.edu.gh>
2023-10-19 12:52:23 +0000

    feat: added obj js

--- 02-Fundamentals-Part-2/obj.js
+++ 02-Fundamentals-Part-2/obj.js
@@ -1,22 +1,48 @@
-const jonasArray = [
-  "Jonas",
-  "Doe",
-  2037 - 1991,
-  "teacher",
-  ["Michael", "Peter", "Steven"],
-];
+// const jonasArray = [
+//   "Jonas",
+//   "Doe",
+//   2037 - 1991,
+//   "teacher",
+//   ["Michael", "Peter", "Steven"],
+// ];
 
-const jonas = {
-  firstName: "Jonas",
-  lastName: "Doe",
-  age: 2037 - 1991,
-  job: "teacher",
-  friends: ["Michael", "Peter", "Steven"],
-};
+// const jonas = {
+//   firstName: "Jonas",
+//   lastName: "Doe",
+//   age: 2037 - 1991,
+//   job: "teacher",
+//   friends: ["Michael", "Peter", "Steven"],
+// };
 
-console.log(jonas);
+// console.log(jonas);
+// console.log(jonas.lastName);
+// console.log(jonas["lastName"]);
+
+// const nameKey = "Name";
+// console.log(jonas["first" + nameKey]);
+// console.log(jonas["last" + nameKey]);
+
+// const interestedIn = prompt(
+//   "What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends"
+// );
+
+// if (jonas[interestedIn]) {
+//   console.log(jonas[interestedIn]);
+// } else {
+//   console.log(
+//     "Wrong request! Choose between firstName, lastName, age, job and friends"
+//   );
+// }
+
+// jonas.location = "Portugal";
+// jonas["twitter"] = "@tkkkkahh";
+// console.log(jonas);
 
 // ===============================================
 //     Bot visualViewport. Bracket Notification
 // ===============================================
-console.log(jonas.firstName);
+// console.log(jonas.firstName);
+
+// // Challenge
+// const jonasFriends = `${jonas.firstName} has ${jonas.friends.length}, and his best friend is called ${jonas.friends[0]}`;
+// console.log(jonasFriends);
