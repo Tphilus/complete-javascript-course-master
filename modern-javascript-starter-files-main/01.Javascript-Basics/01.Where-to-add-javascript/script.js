ba32d7caeffe6aa375dbc3cc0c36a00401a47f5f
Tphilus <tkdadzie@st.ug.edu.gh>
2024-04-26 10:33:14 -0700

    Feat: modern js

---
+++ modern-javascript-starter-files-main/01.Javascript-Basics/01.Where-to-add-javascript/script.js
@@ -0,0 +1,2 @@
+document.getElementById('title').innerText = 'JavaScript';
+			document.getElementById('title').style.color ='red';
---
+++ modern-javascript-starter-files-main/01.Javascript-Basics/02.Async & defer/script1.js
@@ -0,0 +1,2 @@
+document.getElementById("title").innerText = "Javascript";
+document.getElementById("title").style.color = "red";
